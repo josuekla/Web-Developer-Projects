c5af1e4db343bd46849db3d5782007f00e75fc2b
josuekla <josueklayslerjk@gmail.com>
2026-02-28 13:33:49 -0300

    Add files for Drum Kit project including HTML, CSS, JavaScript, images, and audio

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,56 @@
+var buttonList = document.querySelectorAll('.drum')
+buttonList.forEach((button) => button.addEventListener('click', function() {
+    var buttonInnerHTML = this.innerHTML
+    playSound(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML)
+}))
+
+document.addEventListener('keydown', function(event) {
+    playSound(event.key)
+    buttonAnimation(event.key)
+})
+    
+function playSound(key) {
+    switch (key) {
+        case 'w':
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play()
+            break;
+        case 'a':
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play()
+            break;
+        case 's':
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play()
+            break;
+        case 'd':
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play()
+            break;
+        case 'j':
+            var audio = new  Audio('./sounds/snare.mp3');
+            audio.play()
+            break;
+        case 'k':
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play()
+            break;
+        case 'l':
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play()
+            break;
+        
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(button) {
+    let colorChange = document.querySelector(`.${button}`);
+
+    colorChange.classList.add("pressed");
+    setTimeout(function() {
+        colorChange.classList.remove("pressed")
+    }, 300)
+}
