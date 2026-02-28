4b0c9ac814078d6c6a025772118db5b17d975d62
josuekla <josueklayslerjk@gmail.com>
2026-02-28 13:25:01 -0300

    Add initial files for DOM Challenge project including HTML, CSS, and JavaScript

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,6 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "josué"
+
+var a = document.querySelector("li a");
+a.style.color = "red"
+
+document.querySelector("button").style.background = 'yellow'
